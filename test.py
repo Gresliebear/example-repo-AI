35f1aa2e636107103bb1f52c742491f6e1a18d3e
gresliebear <lesliemwubbel@gmail.com>
2024-05-11 21:28:58 -0400

    Initial commit with example.txt

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-import math
-# Generate a list of 10 random integers between 1 and 100
-random_numbers = [random.randint(1, 100) for _ in range(10)]
-
-print("Original list:", random_numbers)
-
+"Hello, World!"
